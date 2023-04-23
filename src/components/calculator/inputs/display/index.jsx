14826ba369722a454366b2ea898e2d1c158ac69a
Gian <givespas@hotmail.com>
2023-04-22 20:34:14 -0500

    feat<input/Display>: prod handleInput

--- src/components/calculator/inputs/display/index.jsx
+++ src/components/calculator/inputs/display/index.jsx
@@ -1,31 +1,31 @@
-import { Button } from '../../button';
+import { Button } from "../../button";
 
-export function InputDisplay() {
+export function InputDisplay({ handleInput }) {
   return (
     <>
-      <Button value="AC" />
-      <Button value="+/-" />
-      <Button value="%" />
-      <Button value="/" color="orange" />
+      <Button value="AC" handleClick={handleInput} />
+      <Button value="+/-" handleClick={handleInput} />
+      <Button value="%" handleClick={handleInput} />
+      <Button value="/" color="orange" handleClick={handleInput} />
       <br />
-      <Button value="7" color="gray" />
-      <Button value="8" color="gray" />
-      <Button value="9" color="gray" />
-      <Button value="*" color="orange" />
+      <Button value="7" color="gray" handleClick={handleInput} />
+      <Button value="8" color="gray" handleClick={handleInput} />
+      <Button value="9" color="gray" handleClick={handleInput} />
+      <Button value="*" color="orange" handleClick={handleInput} />
       <br />
-      <Button value="4" color="gray" />
-      <Button value="5" color="gray" />
-      <Button value="6" color="gray" />
-      <Button value="-" color="orange" />
+      <Button value="4" color="gray" handleClick={handleInput} />
+      <Button value="5" color="gray" handleClick={handleInput} />
+      <Button value="6" color="gray" handleClick={handleInput} />
+      <Button value="-" color="orange" handleClick={handleInput} />
       <br />
-      <Button value="1" color="gray" />
-      <Button value="2" color="gray" />
-      <Button value="3" color="gray" />
-      <Button value="+" color="orange" />
+      <Button value="1" color="gray" handleClick={handleInput} />
+      <Button value="2" color="gray" handleClick={handleInput} />
+      <Button value="3" color="gray" handleClick={handleInput} />
+      <Button value="+" color="orange" handleClick={handleInput} />
       <br />
-      <Button value="0" color="gray" zeroOptional />
-      <Button value="." color="gray" />
-      <Button value="=" color="orange" />
+      <Button value="0" color="gray" handleClick={handleInput} zeroOptional />
+      <Button value="." color="gray" handleClick={handleInput} />
+      <Button value="=" color="orange" handleClick={handleInput} />
     </>
   );
 }
