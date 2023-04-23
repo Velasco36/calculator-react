e550b4669f65348fb90f60af53776fb84d8566ca
Gian <givespas@hotmail.com>
2023-04-22 20:35:38 -0500

    feat<Keyboard>: func keydown

--- src/components/calculator/inputs/keyboard/index.jsx
+++ src/components/calculator/inputs/keyboard/index.jsx
@@ -1,3 +1,29 @@
-export function InputKeyboard() {
-  return <>Keyboard</>;
+import { useEffect } from "react";
+
+export function InputKeyboard({ handleKeydown }) {
+  useEffect(() => {
+    function handleEvent(event) {
+      if (
+        /^[0-9]$/i.test(event.key) ||
+        event.key === "Backspace" ||
+        event.key === "Delete" ||
+        event.key === "Enter" ||
+        event.key === "." ||
+        event.key === "+" ||
+        event.key === "-" ||
+        event.key === "*" ||
+        event.key === "/" ||
+        event.key === "%"
+      ) {
+        handleKeydown(event.key);
+      }
+      return;
+    }
+
+    window.addEventListener("keydown", handleEvent);
+
+    return () => {
+      window.removeEventListener("keydown", handleEvent);
+    };
+  }, [handleKeydown]);
 }
